04008f3a1dbc92e87c7653fae71b5b262988a727
adityy <38124141+adityy@users.noreply.github.com>
2020-03-19 23:43:51 +0530

    Update deployment_pipeline.groovy

--- deployment_pipeline.groovy
+++ deployment_pipeline.groovy
@@ -18,11 +18,8 @@ def main() {
 
                 
                 echo("Options ::")
-                echo("Branch for code checkout : $env.BRANCH_NAME)
-				
-				// Clean up workspace area
-				sh """ rm -rf 
-				"""
+                echo("Branch for code checkout : $env.BRANCH_NAME")
+
 				echo("Checking out the code")
 				gitCheckout("https://github.com/adityy/adityy_sharma.git", env.BRANCH_NAME)
                 
@@ -40,7 +37,7 @@ def main() {
 				sh """
 				echo "Uploading the repository content to the s3 bucket configured for static website hosting"
 			    aws s3 cp index.html s3://aditi-test-bucket-website
-				echo " The website is accesible at http://aditi-test-bucket.s3-website.eu-west-2.amazonaws.com/"
+				echo " The website is accesible at http://test-aditi-bucket.s3-website.eu-west-2.amazonaws.com/"
 				"""
 		}
 	}
@@ -65,7 +62,7 @@ def gitCheckout(def gitURL, def gitBranch){
         submoduleCfg: [],
         userRemoteConfigs: [
             [
-                credentialsId: 'adityy',
+                credentialsId: 'adityy_github',
                 url: "${gitURL}"
             ]
         ]
